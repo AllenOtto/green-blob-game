c235d25b3c6534b90b5310836fb2f027268af410
AllenOtto <kephamoturi@gmail.com>
2023-06-25 19:50:06 +0300

    Drew our blob on the screen with a position of 100*100, a radius of 50, start angle of 0 and end angle of (Math.PI * 2); we then tell our context to fill the circle in with a color green

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const canvas = document.getElementById('game-area');
 const ctx = canvas.getContext('2d');
 
+let x = 100;
+let y = 100;
+let radius = 50;
+
 // Game loop
 function drawGame() {
     clearScreen();
+    drawGreenBlob();
 }
 
+function drawGreenBlob() {
+    ctx.fillStyle = 'green';
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2); // 0 is the start angle
+    // and (Math.PI * 2) is the end angle
+    ctx.fill();
+}
 
 function clearScreen() {
     ctx.fillStyle = 'black';
